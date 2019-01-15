1999ad9967f10bb31eca43c9ddbc5a5608a5291c
CarlisfrinLenovo <car-arejim@hotmail.com>
2019-01-15 15:05:36 +0100

    Added comments

--- Pong/src/app.js
+++ Pong/src/app.js
@@ -16,43 +16,45 @@ var MenuLayer = cc.LayerColor.extend({
 
     init:function(){
 
+        // Creates the buttons
         this.resumeButton = new cc.MenuItemFont("RESUME", this.pressResume, this);
         this.easyButton = new cc.MenuItemFont("CHANGE TO EASY MODE", this.pressEasy, this);
         this.mediumButton = new cc.MenuItemFont("CHANGE TO MEDIUM MODE", this.pressMedium, this);
         this.hardButton = new cc.MenuItemFont("CHANGE TO HARD MODE", this.pressHard, this);
 
+        // Sets the position of the buttons
         this.resumeButton.setPosition(cc.winSize.width/2,cc.winSize.height/2+75);
         this.easyButton.setPosition(cc.winSize.width/2,cc.winSize.height/2+25);
         this.mediumButton.setPosition(cc.winSize.width/2,cc.winSize.height/2-25);
         this.hardButton.setPosition(cc.winSize.width/2,cc.winSize.height/2-75);
 
+        // Creates the menu
         this.menu = new cc.Menu(this.resumeButton, this.easyButton, this.mediumButton, this.hardButton);
         this.menu.setPosition(0,0);
-
         this.addChild(this.menu);
 
         return this;
     },
     pressResume:function () {
-        // Volver a ejecutar la escena Principal
+        // Resumes the game
         this.removeFromParent();
     },
     pressEasy:function () {
-        // Volver a ejecutar la escena Principal
+        // Changes difficulty to easy
         scorePlayer=0;
         scoreCPU=0;
         difficulty = difficulty_level.EASY;
         this.removeFromParent();
     },
     pressMedium:function () {
-        // Volver a ejecutar la escena Principal
+        // Changes difficulty to medium
         scorePlayer=0;
         scoreCPU=0;
         difficulty = difficulty_level.MEDIUM;
         this.removeFromParent();
     },
     pressHard:function () {
-        // Volver a ejecutar la escena Principal
+        // Changes difficulty to hard
         scorePlayer=0;
         scoreCPU=0;
         difficulty = difficulty_level.HARD;
@@ -74,30 +76,36 @@ var GameLayer = cc.Layer.extend({
 
         this._super();
 
+        // Creates ball sprite
         this.spriteBall = cc.Sprite.create( res.ball_png );
         this.spriteBall.setPosition(cc.winSize.width/2,cc.winSize.height/2);
         this.addChild( this.spriteBall );
 
+        // Creates player paddle sprite
         this.spritePlayer = cc.Sprite.create( res.paddle_png );
         this.spritePlayer.setPosition(cc.winSize.width/20,cc.winSize.height/2);
         this.addChild( this.spritePlayer );
 
+        // Creates CPU paddle sprite
         this.spriteCPU = cc.Sprite.create( res.paddle_png );
         this.spriteCPU.setPosition(cc.winSize.width*19/20,cc.winSize.height/2);
         this.addChild( this.spriteCPU );
 
+        // Initialize variables
         this.speedPlayer = 0;
         this.speedCPU = 0;
         this.speedBallX = 4;
         this.speedBallY = 4;
 
+        // Sets the position of the boundaries
         this.upperBound = cc.winSize.height - this.spritePlayer.height;
         this.lowerBound = this.spritePlayer.height;
 
+        // Listener for keyboard
         cc.eventManager.addListener({
               event: cc.EventListener.KEYBOARD,
-              onKeyPressed: this.teclaPulsada,
-              onKeyReleased: this.teclaLevantada
+              onKeyPressed: this.keyDown,
+              onKeyReleased: this.keyUp
         }, this)
 
         this.scheduleUpdate();
@@ -107,16 +115,19 @@ var GameLayer = cc.Layer.extend({
 
 
 
-    teclaPulsada: function ( keyCode, event ) {
-
+    keyDown: function ( keyCode, event ) {
+        // Resume the game in case it was paused
         cc.director.resume();
         if (keyCode==38){
+            // Up key
             event.getCurrentTarget().speedPlayer = 8;
         }
         if (keyCode==40){
+            // Down key
             event.getCurrentTarget().speedPlayer = -8;
         }
         if (keyCode==27){
+            // Escape key, pauses the game and shows the menu
             cc.director.pause();
             var layerMenu = new MenuLayer();
             layerMenu.init();
@@ -124,8 +135,8 @@ var GameLayer = cc.Layer.extend({
         }
     },
 
-    teclaLevantada: function ( keyCode, event ) {
-     
+    keyUp: function ( keyCode, event ) {
+        // When the user stops pressing an arrow, the player paddle stops moving
         if (keyCode==38 || keyCode==40){
             event.getCurrentTarget().speedPlayer = 0;
         }
@@ -133,6 +144,7 @@ var GameLayer = cc.Layer.extend({
 
     score: function (){
         if (this.spriteBall.x > this.spriteCPU.x){
+            // Player scores
             this.spriteBall.setPosition(cc.winSize.width/2,cc.winSize.height/2);
             scorePlayer = scorePlayer + 1;
             if (difficulty==difficulty_level.EASY){
@@ -140,6 +152,7 @@ var GameLayer = cc.Layer.extend({
                 this.speedBallY = 4;
             }
         } else if (this.spriteBall.x < this.spritePlayer.x){
+            // CPU scores
             this.spriteBall.setPosition(cc.winSize.width/2,cc.winSize.height/2);
             scoreCPU = scoreCPU + 1;
             if (difficulty==difficulty_level.EASY){
@@ -153,14 +166,15 @@ var GameLayer = cc.Layer.extend({
 
     update: function (){
 
-        // Movimiento jugador
+        // Player movement
         if (((this.spritePlayer.y + this.speedPlayer + this.spritePlayer.height/2) < this.upperBound) && ((this.spritePlayer.y + this.speedPlayer - this.spritePlayer.height/2) > this.lowerBound)) {
             this.spritePlayer.y = this.spritePlayer.y + this.speedPlayer;
         }
 
-        // Movimiento CPU
+        // CPU movement depends on difficulty
         switch (difficulty){
             case difficulty_level.EASY:
+                // CPU moves the same direction that the ball is moving
                 if (this.speedBallX<0){
                     this.speedCPU = 0;
                 } else if (this.speedBallY>0){
@@ -172,6 +186,7 @@ var GameLayer = cc.Layer.extend({
                 }
                 break;
             case difficulty_level.MEDIUM:
+                // CPU moves to the place where the ball is
                 if (this.speedBallX<0){
                     this.speedCPU = 0;
                 } else if (this.spriteBall.y>this.spriteCPU.y){
@@ -183,6 +198,7 @@ var GameLayer = cc.Layer.extend({
                 }
                 break;
             case difficulty_level.HARD:
+                // CPU moves like in medium but tries to hit the ball with the side
                 break;
         }
         if (((this.spriteCPU.y + this.speedCPU + this.spriteCPU.height/2) < this.upperBound) && ((this.spriteCPU.y + this.speedCPU - this.spriteCPU.height/2) > this.lowerBound)) {
@@ -190,12 +206,12 @@ var GameLayer = cc.Layer.extend({
         }
 
 
-        // Colisión pelota-pared
+        // Collision between ball and wall
         if (((this.spriteBall.y + this.speedBallY + this.spriteBall.height/2) > this.upperBound) || ((this.spriteBall.y + this.speedBallY - this.spriteBall.height/2) < this.lowerBound)) {
             this.speedBallY = -1 * this.speedBallY;
         }
 
-        // Colisión pelota-barra
+        // Collision between ball and paddles
         var areaBall = this.spriteBall.getBoundingBox();
         var areaPlayer = this.spritePlayer.getBoundingBox();
         var areaCPU = this.spriteCPU.getBoundingBox();
@@ -207,11 +223,11 @@ var GameLayer = cc.Layer.extend({
             this.speedBallX =  -1 * this.speedBallX;
         }
 
-        // Movimiento pelota
+        // Ball movement
         this.spriteBall.x = this.spriteBall.x + this.speedBallX;
         this.spriteBall.y = this.spriteBall.y + this.speedBallY;
 
-        // Punto
+        // Score detection
         this.score();
 
     }
